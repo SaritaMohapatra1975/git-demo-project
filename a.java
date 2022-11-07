3eeb80c5c13f6031f94c3711b23104114c017afc
SaritaMohapatra1975 <113372375+SaritaMohapatra1975@users.noreply.github.com>
2022-11-07 11:30:50 +0530

    Create a.java

---
+++ a.java
@@ -0,0 +1,15 @@
+public class A
+{
+public static void main(String[] args)
+{
+int i, num =10, sum = 0;
+//executes until the condition returs true
+for(i = 1; i <= num; ++i)
+{
+//adding the value of 1 into sum variable
+sum = sum + i;
+}
+//prints the sum
+System.out.println("Sum of 10 Natural Numbers is = " + sum);
+}
+}
